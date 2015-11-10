00697290bd9db851dded26bfafefcc863565b5e9
Philip Johnson <johnson@hawaii.edu>
2015-11-09 16:14:15 -1000

    CAS working, now fix the UI

--- app/server/seeds/Accounts.js
+++ app/server/seeds/Accounts.js
@@ -1,4 +1,4 @@
-/* When running app for first time, pass a settings file to set up a default user account. */
+/* When running app for first time, pass a settings file to set up a default user account.
 if (Meteor.users.find().count() === 0) {
   if (!!Meteor.settings.defaultAccount) {
     Accounts.createUser({
@@ -10,3 +10,22 @@ if (Meteor.users.find().count() === 0) {
     console.log("No default user!  Please invoke meteor with a settings file.");
   }
 }
+
+*/
+
+/* Validate username, sending a specific error message on failure. */
+Accounts.validateNewUser(function (user) {
+  console.log("validating new user: " + user);
+  if (user) {
+    var username = user.services.cas.id;
+    console.log("validating username: " + username);
+    console.log(Meteor.settings.allowed_users);
+    if (username && _.contains(Meteor.settings.allowed_users,username)) {
+      console.log("user allowed!")
+      return true;
+    }
+  }
+  console.log("user not allowed!");
+  throw new Meteor.Error(403, "User not in the allowed list");
+  //return false;
+});
