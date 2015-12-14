4125a220f60b821efab5871775aec6dcce36f1d6
Josephine Garces <josebeann@gmail.com>
2015-12-13 21:02:17 -1000

    Added Images collection git tracking and set validation to false

---
+++ app/lib/collections/Images.js
@@ -0,0 +1,52 @@
+Images = new Mongo.Collection('Images');
+
+Images.attachSchema(new SimpleSchema({
+  image: {
+    type: String,
+    optional: true,
+    autoform: {
+      afFieldInput: {
+        type: 'cloudinary'
+      }
+    }
+  },
+
+  user:{
+    type: String,
+    optional: true
+  },
+
+  firstName:{
+    type: String,
+    optional: true
+  },
+
+
+  lastName:{
+    type: String,
+    optional: true
+  }
+}));
+
+// Publish the entire Collection.  Subscription performed in the router.
+if (Meteor.isServer) {
+  Meteor.publish('Images', function () {
+    return Images.find();
+  });
+}
+
+Meteor.methods({
+
+  insertImage: function(doc){
+    doc.user = Meteor.user().profile.name;
+    Images.insert(doc);
+  },
+
+  editImage: function(doc){
+    check(doc, Images.simpleSchema());
+    Images.update({user: Meteor.user().profile.name}, {$set: {firstName: doc.firstName}}, {validate: false});
+    Images.update({user: Meteor.user().profile.name}, {$set: {lastName: doc.lastName}}, {validate: false});
+    Images.update({user: Meteor.user().profile.name}, {$set: {image: doc.image}}, {validate: false});
+  }
+
+});
