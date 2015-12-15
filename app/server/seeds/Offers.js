5b49552314a168dad2bf2767f0806f68e617205e
mkshimod <mkshimod@hawaii.edu>
2015-12-14 23:44:13 -1000

    added expiration dates and admin names to seeds

--- app/server/seeds/Offers.js
+++ app/server/seeds/Offers.js
@@ -1,27 +1,36 @@
 
 
+
+
 var buyoffersSeeds = [
-  {title:"Java Concepts: Compatible with Java 5, 6 and 7", ISBN10: "", ISBN13:"", condition:"Good", price:5, offerType:"Buy", creator:"Mil", accepted: false},
-  {title:"Java Concepts: Compatible with Java 5, 6 and 7", ISBN10:"", ISBN13: "", condition:"Good", price:5, offerType:"Buy", creator:"jgarces", seller:"Jack", accepted: true},
-  {title:"Discrete Mathematics and Its Applications Seventh Edition", ISBN10:"", ISBN13: "", condition:"Excellent", price:10, offerType:"Buy", creator:"Jill", accepted: false},
-  {title:"The C Programming Language", ISBN10: "", ISBN13: "", condition:"Good", price:10, offerType:"Buy", creator:"Mary", accepted: false},
-  {title:"Introduction to Algorithms, 3rd Edition", ISBN10: "", ISBN13: "", condition:"Excellent", price:5, offerType:"Buy", creator:"Anne", accepted: false},
-  {title:"Digital Logic Design: A Rigorous Approach", ISBN10: "", ISBN13: "", condition:"Excellent", price:5, offerType:"Buy", creator:"Hillary", accepted: false},
-  {title:"Ethics for the Information Age (5th Edition)", ISBN10:"", ISBN13: "", condition:"Good", price:20, offerType:"Buy", creator:"Dawn", accepted: false},
-  {title:"Artificial Intelligence for Games. CRC Press. 2009.", ISBN10: "", ISBN13: "", condition:"Excellent", price:100, offerType:"Buy", creator:"Tracy", accepted: false},
-  {title:"Programming Language Pragmatics, Third Edition", ISBN10: "", ISBN13: "", condition:"Good", price:"250", offerType:"Buy", creator:"Stephanie", accepted: false}
+  {title:"Java Concepts: Compatible with Java 5, 6 and 7", ISBN10: "", ISBN13:"", condition:"Good", price:5, offerType:"Buy", creator:"jgarces", accepted: false, expiration: new Date('December 17, 1995 03:24:00')},
+
+  {title:"Java Concepts: Compatible with Java 5, 6 and 7", ISBN10:"", ISBN13: "", condition:"Good", price:5, offerType:"Buy", creator:"mkshimod", seller:"Jack", accepted: true, expiration: new Date('January 20, 2016 17:44:00')},
+
+  {title:"Discrete Mathematics and Its Applications Seventh Edition", ISBN10:"", ISBN13: "", condition:"Excellent", price:10, offerType:"Buy", creator:"mkshimod", accepted: false, expiration: new Date('December 15, 2015 05:34:00')},
+
+  {title:"The C Programming Language", ISBN10: "", ISBN13: "", condition:"Good", price:10, offerType:"Buy", creator:"mkshimod", accepted: false, expiration: new Date('December 17, 2015 15:00:00')},
+
+  {title:"Introduction to Algorithms, 3rd Edition", ISBN10: "", ISBN13: "", condition:"Excellent", price:5, offerType:"Buy", creator:"mkshimod", accepted: false, expiration: new Date('December 15, 2015 19:30:00')},
+
+  {title:"Digital Logic Design: A Rigorous Approach", ISBN10: "", ISBN13: "", condition:"Excellent", price:5, offerType:"Buy", creator:"mkshimod", accepted: false, expiration: new Date('December 16, 2015 15:00:00')},
+
+  {title:"Ethics for the Information Age (5th Edition)", ISBN10:"", ISBN13: "", condition:"Good", price:20, offerType:"Buy", creator:"jgarces", accepted: false, expiration: new Date('December 20, 2015 00:00:00')},
+
+  {title:"Artificial Intelligence for Games. CRC Press. 2009.", ISBN10: "", ISBN13: "", condition:"Excellent", price:100, offerType:"Buy", creator:"chumeda", accepted: false, expiration: new Date('December 25, 2015 00:00:00')},
+
+  {title:"Programming Language Pragmatics, Third Edition", ISBN10: "", ISBN13: "", condition:"Good", price:"250", offerType:"Buy", creator:"chumeda", accepted: false, expiration: new Date('December 15, 2015 01:34:00')}
 ];
 
 var selloffersSeeds = [
-  {title:"Java Concepts: Compatible with Java 5, 6 and 7", ISBN10: "", ISBN13: "", condition:"Good", price:"20", offerType:"Sell", creator:"Nick", accepted: false},
-  {title:"Java Concepts: Compatible with Java 5, 6 and 7", ISBN10: "", ISBN13: "", condition:"Good", price:5, offerType:"Buy", creator:"Jack", accepted: true},
-  {title:"Discrete Mathematics and Its Applications Seventh Edition", ISBN10: "", ISBN13: "", condition:"Excellent", price:"100", offerType:"Sell", creator:"Sam", accepted: false},
-  {title:"The C Programming Language", ISBN10: "", ISBN13: "", condition:"Excellent", price:"25", offerType:"Sell", creator:"Karen", accepted: false},
-  {title:"Introduction to Algorithms, 3rd Edition", ISBN10: "", ISBN13: "", condition:"Good", price:"50", offerType:"Sell", creator:"Amber", accepted: false},
-  {title:"Digital Logic Design: A Rigorous Approach", ISBN10: "", ISBN13: "", condition:"Excellent", price:"100", offerType:"Sell", creator:"Sean", accepted: false},
-  {title:"Ethics for the Information Age (5th Edition)", ISBN10:"", ISBN13: "", condition:"Good", price:"25", offerType:"Sell", creator:"Zack", accepted: false},
-  {title:"Artificial Intelligence for Games. CRC Press. 2009.", ISBN10:"", ISBN13: "", condition:"Excellent", price:"75", offerType:"Sell", creator:"Sai", accepted: false},
-  {title:"Programming Language Pragmatics, Third Edition", ISBN10: "", ISBN13: "", condition:"Good", price:"1", offerType:"Sell", creator:"Lan", accepted: false}
+  {title:"Java Concepts: Compatible with Java 5, 6 and 7", ISBN10: "", ISBN13: "", condition:"Good", price:"20", offerType:"Sell", creator:"chumeda", accepted: false, expiration: new Date('December 15, 2015 05:34:00')},
+  {title:"Discrete Mathematics and Its Applications Seventh Edition", ISBN10: "", ISBN13: "", condition:"Excellent", price:"100", offerType:"Sell", creator:"chumeda", accepted: false, expiration: new Date('December 31, 2015 11:00:00')},
+  {title:"The C Programming Language", ISBN10: "", ISBN13: "", condition:"Excellent", price:"25", offerType:"Sell", creator:"chumeda", accepted: false, expiration: new Date('February 10, 2015 00:30:00')},
+  {title:"Introduction to Algorithms, 3rd Edition", ISBN10: "", ISBN13: "", condition:"Good", price:"50", offerType:"Sell", creator:"chumeda", accepted: false, expiration: new Date('December 15, 2015 05:34:00')},
+  {title:"Digital Logic Design: A Rigorous Approach", ISBN10: "", ISBN13: "", condition:"Excellent", price:"100", offerType:"Sell", creator:"chumeda", accepted: false, expiration: new Date('December 20, 2015 05:34:00')},
+  {title:"Ethics for the Information Age (5th Edition)", ISBN10:"", ISBN13: "", condition:"Good", price:"25", offerType:"Sell", creator:"mkshimod", accepted: false, expiration: new Date('December 15, 2015 11:34:00')},
+  {title:"Artificial Intelligence for Games. CRC Press. 2009.", ISBN10:"", ISBN13: "", condition:"Excellent", price:"75", offerType:"Sell", creator:"mkshimod", accepted: false, expiration: new Date('December 20, 2015 05:34:00')},
+  {title:"Programming Language Pragmatics, Third Edition", ISBN10: "", ISBN13: "", condition:"Good", price:"1", offerType:"Sell", creator:"mkshimod", accepted: false, expiration: new Date('December 15, 2015 05:34:00')}
 
 ];
 
